8bedfdd23b5c9404bde2e1cde5e1921b2ec86670
Adipati Ma'ruf Alamsyah <adipati.cs.samsung@gmail.com>
2021-03-21 03:12:10 +0700

    cropped left eye

--- FacialLandmark.py
+++ FacialLandmark.py
@@ -2,13 +2,20 @@ import cv2
 import numpy as np
 import dlib
 
-img = cv2.imread("Bae Suzy.jpg")
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('./shape_predictor_68_face_landmarks.dat')
+
+def createBox(img, points, scale = 5):
+  bBox = cv2.boundingRect(points)
+  x, y, w, h = bBox
+  imgCrop = img[y-5:y+h+5, x-5:x+w+5]
+  imgCrop = cv2.resize(imgCrop, (0, 0), None, scale, scale)
+  return imgCrop
+
+
+img = cv2.imread('Bae Suzy.jpg')
 img = cv2.resize(img, (0, 0), None, 0.5, 0.5)
 imgOriginal = img.copy()
-
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("./shape_predictor_68_face_landmarks.dat")
-
 imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces = detector(imgGray)
 
@@ -17,12 +24,18 @@ for face in faces:
   x2, y2 = face.right(), face.bottom()
   imgOriginal = cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
   landmarks = predictor(imgGray, face)
+  myPoints = []
   for n in range(68):
     x = landmarks.part(n).x
     y = landmarks.part(n).y
-    cv2.circle(imgOriginal, (x, y), 4, (50, 50, 255), cv2.FILLED)
-    cv2.putText(imgOriginal, str(n), (x, y-10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0, 0, 255), 1)
+    myPoints.append([x, y])
+    cv2.circle(imgOriginal, (x, y), 2, (50, 50, 255), cv2.FILLED)
+    # cv2.putText(imgOriginal, str(n), (x, y-10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0, 0, 255), 1)
   
+  myPoints = np.array(myPoints)
+  imgLeftEye = createBox(img, myPoints[36:42])
+  cv2.imshow('Left Eye', imgLeftEye)
+  print(myPoints)
 
-cv2.imshow("Original", imgOriginal)
+cv2.imshow('Original', imgOriginal)
 cv2.waitKey(0)
